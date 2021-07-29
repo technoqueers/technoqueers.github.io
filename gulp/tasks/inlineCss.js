ec50b7a092ce88d25d71c7b3615b4a96c8ae4be9
Brad Osgood <bosgood@gmail.com>
2021-07-29 09:54:40 -0400

    Site redesign for 2021

--- gulp/tasks/inlineCss.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-    inlineCss = require('gulp-inline-css');
-
-gulp.task('inlineCss', function() {
-    return gulp.src('newsletter.html')
-        .pipe(inlineCss())
-        .pipe(gulp.dest('./'));
-});
